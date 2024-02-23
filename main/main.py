c2eddb97ede4deefb8b8895865dbf228b0cd78e2
Pavel Gornostaev <pavelgornostaeff@yandex.ru>
2024-02-23 14:00:37 +0300

    Добавил функцию main

---
+++ main/main.py
@@ -0,0 +1,12 @@
+from utils import (receive_transactions, get_exec_transcriptions, sort_with_date, show_transcriptions)
+
+
+def main():
+    executed_transaction = get_exec_transcriptions(receive_transactions())
+    sorted_transaction = sort_with_date(executed_transaction)
+    for i in sorted_transaction:
+        print(show_transcriptions(i))
+
+
+if __name__ == '__main__':
+    main()
